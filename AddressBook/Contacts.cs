7530601261151453185205b1770f68ee16e3cf6a
Akash <akashmane12598@gmail.com>
2020-09-30 23:50:21 +0530

    [Akash] Add. Delete Contact UC4

--- AddressBook/Contacts.cs
+++ AddressBook/Contacts.cs
@@ -6,8 +6,6 @@ namespace AddressBook
 {
     class Contacts
     {
-        List<Contacts> contacts = new List<Contacts>();
-
         public string first_name;
         public string last_name;
         public string address;
@@ -16,11 +14,6 @@ namespace AddressBook
         public int zip;
         public long phone;
         public string email;
-
-        public Contacts()
-        {
-
-        }
 
         public Contacts(string first_name, string last_name, string address, string city, string state, int zip, long phone, string email)
         {
@@ -32,21 +25,6 @@ namespace AddressBook
             this.zip = zip;
             this.phone = phone;
             this.email = email;
-        }
-
-        public void setContacts(List<Contacts> contacts)
-        {
-            this.contacts = contacts;
-        }
-
-        public List<Contacts> getContacts()
-        {
-            return contacts;
-        }
-
-        public List<Contacts> showContacts()
-        {
-            return contacts;
         }
 
         public override string ToString()
